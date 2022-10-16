a22240111dd75ff2a00061b5d18d0f21df001df8
vvihorev <vvihorevv@gmail.com>
2022-10-16 23:27:20 +0200

    Added primitive spacy text cleansing

--- sergio_text_processing/main.py
+++ sergio_text_processing/main.py
@@ -1,12 +1,17 @@
+import re
+
 import spacy
 
 
 nlp = spacy.load("ru_core_news_sm")
 
-with open("sample_questions", "r", encoding="utf-8") as file:
-    lines = file.readlines()
-for line in lines:
-    # print(token.text, token.pos_, token.dep_)
-    res = [token.lemma_ for token in nlp(line) if not token.is_stop]
-    print(res)
 
+def get_question_tokens(question: str) -> list:
+    question = re.sub(r'[^ \w]|\d', '', question)
+    return [token.lemma_ for token in nlp(question) if not token.is_stop]
+
+
+tokens = get_question_tokens("Пожалуйста, помогить установить python 3.8.10 себе на комп.")
+faq = {"Пожалуйста, напишите администраторам.": ["загружать", "отправить", "открыть", "установить"]}
+for value in faq.values():
+    print(len(set(value) & set(tokens)))
