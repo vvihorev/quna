3e5f74a26e03a09ddb768871010ee5863ee73157
vvihorev <vvihorevv@gmail.com>
2022-10-16 19:47:06 +0200

    Base selenium funcitons for testing

--- qna_bot.py
+++ qna_bot.py
@@ -67,28 +67,24 @@ def send_answer() -> None:
     driver.find_element(By.XPATH, "//*[contains(text(), 'Отправить')]").click()
 
 
-class MyApp(App):
-    """An example of a very simple Textual App"""
+# JUST IGNORE THIS CODE
+# class MyApp(App):
+#     """An example of a very simple Textual App"""
 
-    async def on_load(self, event: events.Load) -> None:
-        """Bind keys with the app loads (but before entering application mode)"""
-        await self.bind("b", "view.toggle('sidebar')", "Toggle sidebar")
-        await self.bind("q", "quit", "Quit")
-        await self.bind("escape", "quit", "Quit")
+#     async def on_load(self, event: events.Load) -> None:
+#         """Bind keys with the app loads (but before entering application mode)"""
+#         await self.bind("b", "view.toggle('sidebar')", "Toggle sidebar")
+#         await self.bind("q", "quit", "Quit")
+#         await self.bind("escape", "quit", "Quit")
 
-    async def on_mount(self, event: events.Mount) -> None:
-        """Create and dock the widgets."""
-
-        # A scrollview to contain the markdown file
-        body = ScrollView(gutter=1)
-
-        # Header / footer / dock
-        await self.view.dock(Header(), edge="top")
-        await self.view.dock(Footer(), edge="bottom")
-        await self.view.dock(Placeholder(), edge="left", size=30, name="sidebar")
-
-        # Dock the body in the remaining space
-        await self.view.dock(body, edge="right")
-
-
-MyApp.run(title="Simple App")
+#     async def on_mount(self, event: events.Mount) -> None:
+#         """Create and dock the widgets."""
+#         # A scrollview to contain the markdown file
+#         body = ScrollView(gutter=1)
+#         # Header / footer / dock
+#         await self.view.dock(Header(), edge="top")
+#         await self.view.dock(Footer(), edge="bottom")
+#         await self.view.dock(Placeholder(), edge="left", size=30, name="sidebar")
+#         # Dock the body in the remaining space
+#         await self.view.dock(body, edge="right")
+# MyApp.run(title="Simple App")
